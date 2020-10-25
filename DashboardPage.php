3e04e01b7ba76c5654886afab69fdc24880c7cab
zaimsyaddad99 <zaimibnusyam@gmail.com>
2020-10-25 11:01:31 +0700

    dashboard page

---
+++ DashboardPage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' type='text/css' media='screen' href='css/DashboardPage.css'>
+    <title>Dashboard</title>
+</head>
+<body>
+
+    <!--- Navigation Bar --->
+    <nav>
+        <a href="#">Home</a>
+        <a href="#">History</a>
+        <div class="search">
+            <form action="">
+                <input type="search" placeholder="Search" required>
+            </form>
+        </div>
+        <a href="#">Log out</a>
+
+    </nav>
+    
+</body>
+</html>
